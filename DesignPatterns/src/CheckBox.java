a9b7fc6038117b391475888dd4b07914808e2e37
Yulia Mikhaylova <ymik0410@gmail.com>
2022-05-19 17:12:48 +0200

    Polymorphism - taking on many different forms

---
+++ DesignPatterns/src/CheckBox.java
@@ -0,0 +1,8 @@
+public class CheckBox extends UIControl {
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a checkbox");        
+    }
+    
+}
--- DesignPatterns/src/TextBox.java
+++ DesignPatterns/src/TextBox.java
@@ -1,3 +1,8 @@
 public class TextBox extends UIControl {
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a textbox");        
+    }
     
 }
